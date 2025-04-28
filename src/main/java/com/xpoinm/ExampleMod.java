2ea3832f30f2da2595cc3b2a8783ccde46ab25ba
xpoinm2 <_@example.com>
2025-04-28 20:56:46 +0300

    Initial commit: добавил проект Fabric мод xpoinm

--- src/main/java/com/xpoinm/ExampleMod.java
+++ src/main/java/com/xpoinm/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.xpoinm;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+	public static final String MOD_ID = "xpoinm";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
